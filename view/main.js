4cb0706e4021b64e2f966e5b7a711c4efe124896
K-L Reinhard <ui-kreinhard>
2014-05-31 20:55:42 +0200

    extracted routing into own file

--- view/main.js
+++ view/main.js
@@ -20,36 +20,3 @@ app.factory('authHttpInterceptor', function($q, $location) {
 }).config(function($provide, $httpProvider) {
     return $httpProvider.interceptors.push('authHttpInterceptor');
 });
-
-
-
-app.config(function($routeProvider) {
-    $routeProvider.when('/',
-            {
-                templateUrl: 'welcome.html'
-            }
-    ).
-            when('/table/:viewName',
-            {
-                templateUrl: 'tableview.html',
-                controller: 'gridController'
-            }).
-            when('/autoform/:viewName/:id',
-            {
-                controller: 'autoFormController',
-                templateUrl: 'autoForm.html'
-            }).
-            when('/autoform/:viewName',
-            {
-                controller: 'autoFormController',
-                templateUrl: 'autoForm.html'
-            }).
-            when('/welcome', {
-        templateUrl: 'welcome.html'
-    }).
-            when('/login',
-            {
-                controller: 'loginController',
-                templateUrl: 'login.html'
-            });
-});
---
+++ view/routing.js
@@ -0,0 +1,32 @@
+app.config(function($routeProvider) {
+    $routeProvider.when('/',
+            {
+                templateUrl: 'welcome.html'
+            }
+    ).
+            when('/table/:viewName',
+            {
+                templateUrl: 'tableview.html',
+                controller: 'gridController'
+            }).
+            when('/autoform/:viewName/:id',
+            {
+                controller: 'autoFormController',
+                templateUrl: 'autoForm.html'
+            }).
+            when('/autoform/:viewName',
+            {
+                controller: 'autoFormController',
+                templateUrl: 'autoForm.html'
+            }).
+            when('/welcome', 
+            {
+                templateUrl: 'welcome.html'
+            }). 
+            when('/login',
+            {
+                controller: 'loginController',
+                templateUrl: 'login.html'
+            });
+});
+
