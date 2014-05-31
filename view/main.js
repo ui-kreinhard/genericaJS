2cc5a640a9705863174b3b78637f526bf33534b6
K4L <kl.reinhard@googlemail.com>
2014-05-31 13:48:24 +0200

    cleanup code

--- view/main.js
+++ view/main.js
@@ -3,25 +3,23 @@ var app = angular.module('generica', ['ngGrid', 'ngRoute', 'ngResource', 'mgcrea
 
 
 
- app.factory('authHttpInterceptor', function($q, $location) {
-        return {
-            // optional method
-            'response': function(response) {
-                // do something on success
-                return response || $q.when(response);
-            },
-          
-           'responseError': function(rejection) {
-                // do something on error
-                if($location.$$url!='/login') {
-                    $location.path('/login');
-                }
-                return $q.reject(rejection);
+app.factory('authHttpInterceptor', function($q, $location) {
+    return {
+        'response': function(response) {
+            // default handling
+            return response || $q.when(response);
+        },
+        'responseError': function(rejection) {
+            // prevent redirect loop
+            if ($location.$$url != '/login') {
+                $location.path('/login');
             }
-        };
-    }).config(function($provide, $httpProvider) {
-        return $httpProvider.interceptors.push('authHttpInterceptor');
-    });
+            return $q.reject(rejection);
+        }
+    };
+}).config(function($provide, $httpProvider) {
+    return $httpProvider.interceptors.push('authHttpInterceptor');
+});
 
 
 
