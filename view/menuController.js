687961dd06d3ab9416b60467969bc8c269d4c431
K-L Reinhard <ui-kreinhard>
2014-06-02 08:44:58 +0200

    implemented dynamic menu with controller etc.

--- view/menuController.js
+++ view/menuController.js
@@ -9,7 +9,18 @@ app.controller('menuController', function($scope, $http, $routeParams) {
 			hasNoLink: function() {
 				return true;
 			},
-			children: []
+			children: [
+				{
+					menuLabel: 'bla',
+					link: 'asdf',
+					hasLink: function() {
+						return true;
+					}
+					, hasNoLink: function() {
+						return false;
+					}
+				}
+			]
 		},
                {
                         menulabel: 'Worktimes2',
@@ -23,6 +34,46 @@ app.controller('menuController', function($scope, $http, $routeParams) {
                         },
 
                 }
-
+		
 	];
+	$scope.menuItems = [];
+	$http({
+		method: 'GET',
+		url: '../readout_table',
+		params: {
+			pageSize: '1000',
+			page: 1,
+			tableName: 'menu'
+		}
+	}).success(function(data, status, headers, config) {
+		var untransformedData = data.data;
+		var idMappedData = {};
+		angular.forEach(untransformedData, function(value,key) {
+			value.children = [];
+			value.hasLink = function() {
+				return this.children.length <= 0;
+			};
+			value.hasNoLink = function() {
+				return !this.hasLink();
+			};
+			idMappedData[value.id] = value;
+		});
+		angular.forEach(idMappedData, function(value, key) {
+			if(value.menu_idmenu!=null && value.menu_idmenu!='') {
+				var parent = idMappedData[value.menu_idmenu];
+				parent.children.push({
+					text: value.menulabel,
+					href: value.link
+				});
+			}
+		});
+		angular.forEach(idMappedData, function(value, key) {
+			if(value.children.length > 0) {
+				$scope.menuItems.push(value);
+			}
+		});
+	}).error(function(data, status, headers, config) {
+		console.log('cannot load menu data');
+		console.log(status);
+	});
 });
