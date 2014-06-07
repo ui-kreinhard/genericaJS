99dedc2464105280f7fb0b6920f8ceebee54cb6e
K-L Reinhard <ui-kreinhard>
2014-06-07 12:04:31 +0200

    missing test

---
+++ tests/testValidationController.js
@@ -0,0 +1,19 @@
+var username = 'postgres';
+var password = 'hieWueS8';
+var config = require('../config.js');
+var connection = require('../dbHandler.js');
+var dbHandler = connection.dbHandler(username, password, function() {
+	console.log('connection failure');
+}, function() {
+	var dataDao = require('../dataDao.js').dataDao(dbHandler);
+	var validationController = require('../validationController.js').validatonController(dataDao);
+	validationController.validate({
+		tableName: 'worktimes',
+		values: {
+			date_start: '2013',
+			date_end: '2012'
+		}
+	});
+});
+
+
