2f98e50a1f685837a99249de76a6d4b7866e13bc
K4L <kl.reinhard@googlemail.com>
2014-08-10 11:16:24 +0200

    added all crud variations of table to routing edit and delete selection is not finished yet

--- view/routing.js
+++ view/routing.js
@@ -2,11 +2,50 @@ app.config(function($routeProvider) {
     $routeProvider.when('/',
             {
                 templateUrl: 'welcome.html'
-            }
-    ).
+            }).
             when('/table/:viewName',
             {
                 templateUrl: 'tableview.html',
+                controller: 'gridController'
+            }).
+	    when('/table_cr/:viewName',
+	    {
+		templateUrl: 'tableviewCr.html',
+		controller: 'gridController'
+            }).
+  	    when('/table_cru/:viewName',
+            {
+                templateUrl: 'tableviewCru.html',
+                controller: 'gridController'
+            }).
+	    when('/table_crud/:viewName',
+            {
+                templateUrl: 'tableviewCrud.html',
+                controller: 'gridController'
+            }).
+            when('/table_crd/:viewName',
+            {
+                templateUrl: 'tableviewCrd.html',
+                controller: 'gridController'
+            }).
+            when('/table_crud/:viewName',
+            {
+                templateUrl: 'tableviewCrud.html',
+                controller: 'gridController'
+            }).
+            when('/table_d/:viewName',
+            {
+                templateUrl: 'tableviewD.html',
+                controller: 'gridController'
+            }).
+            when('/table_u/:viewName',
+            {
+                templateUrl: 'tableviewU.html',
+                controller: 'gridController'
+            }).
+            when('/table_ud/:viewName',
+            {
+                templateUrl: 'tableviewUd.html',
                 controller: 'gridController'
             }).
             when('/autoform/:viewName/:id',
